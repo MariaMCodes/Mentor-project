2de77dfe41e4362bcdea67431cea05d3ce3fa472
MMempin421 <maria.mempin@hotmail.com>
2021-02-15 13:37:17 +1100

    Using Version Control and Ignoring Stuff

--- Scripts/First commit.R
+++ Scripts/First commit.R
@@ -1,3 +1,12 @@
-## To enable GitHub to attribute all of our commits
+### To enable GitHub to attribute all of our commits
 system('git config -- globaluser.email"maria.mempin@mq.edu.au"')
 system('git config -- globaluser.name"MariaMempin"')
+## This setup step only needs to be done once on any new device
+
+
+### Using Version Control
+## See notes
+
+
+### Ignoring Stuff
+## To ensure GitHub does not track our raw data files
